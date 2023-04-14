a2b1299fa85aac2f7904332cbe5b62e28cee9f38
vairam <vairavel.k22@gmail.com>
2023-04-14 19:03:38 +0530

    new lines1

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function ssk(){
+    var res=document.getElementById("first").value;
+    var res1=document.getElementById("middle").value;
+    var res2=document.getElementById("last").value;
+    var res3=document.getElementById("email").value;
+
+    console.log(res);
+    console.log(res1);
+    console.log(res2);
+    console.log(res3);
+}
